f5f99eaf838e2a61bb820c66732319285b525e68
Nkoa Christophe <nkoachristophe@gmail.com>
2021-04-26 17:25:57 +0100

    test2

--- run.py
+++ run.py
@@ -2,5 +2,10 @@ import test
 
 if __name__ == "__main__":
     response = test.complete_return_request(1254, "Chris", "ER343", "")
-    print(response.text)
-    print(response.status_code)
+    # print(response.text)
+    # print(response.status_code)
+
+    if response.status_code in [200, 201, 202]:
+        print("Cancel invoice is a success")
+    else:
+        print("error attempted during the processing")
